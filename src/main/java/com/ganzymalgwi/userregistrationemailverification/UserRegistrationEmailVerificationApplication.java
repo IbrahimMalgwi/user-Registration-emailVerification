a16d405512a8850c577fcfba6d60077d53429b41
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-12 08:52:50 +0100

    spring boot user registration with security and email verification

---
+++ src/main/java/com/ganzymalgwi/userregistrationemailverification/UserRegistrationEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.ganzymalgwi.userregistrationemailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationEmailVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganzymalgwi/userregistrationemailverification/UserRegistrationEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzymalgwi.userregistrationemailverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationEmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
