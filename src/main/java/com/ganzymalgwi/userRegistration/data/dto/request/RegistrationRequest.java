7e71aaa1ab2a0ef3e0f5b4f17496073cd1503631
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-13 21:17:08 +0100

    refactored

--- src/main/java/com/ganzymalgwi/userRegistration/data/dto/request/RegistrationRequest.java
+++ src/main/java/com/ganzymalgwi/userRegistration/data/dto/request/RegistrationRequest.java
@@ -1,10 +1,13 @@
 package com.ganzymalgwi.userRegistration.data.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
 public class RegistrationRequest {
     private String firstName;
     private String lastName;
